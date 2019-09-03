2bccae1bc97ab09ed58e9995b133bdbed6eec90a
Thiago Dantas <thiagodma52@gmail.com>
2019-09-03 16:08:27 -0300

    Adicionadas funcionalidades para a biblioteca

--- bib.py
+++ bib.py
@@ -1,102 +1,124 @@
 import pandas as pd
 import numpy as np
 import re
+import os
+from sklearn.tree import DecisionTreeClassifier
 
-def calculate_cost_of_decisions(y_pred, costs):
-    cost = 0
-    for i in range(len(y_pred)):
-        if(y_pred[i] == 0):
-            cost = cost + costs.iloc[i,0]
-        else:
-            cost = cost + costs.iloc[i,1]
-    return cost
+class Data():
 
-def separate_qp(qp_val, qp_train, qp_test, X_train, X_test, y_train, y_test, costs_train, costs_test):
-    qp_set =[]
-    if qp_val == 22:
-        qp_set = [23,24,25]
-    elif qp_val == 27:
-        qp_set = [28,29,30]
-    elif qp_val == 32:
-        qp_set = [33,34,35]
-    elif qp_val == 37:
-        qp_set = [38,39,40]
+    def __init__(self, simplify_classes:bool=True):
+        self.features_train = pd.DataFrame()
+        self.costs_train = pd.DataFrame()
+        self.classes_train = pd.DataFrame()
+        self.features_valid = pd.DataFrame()
+        self.costs_valid = pd.DataFrame()
+        self.classes_valid = pd.DataFrame()
+        self.simplify_classes = simplify_classes
 
-    idxs0 = np.where(qp_train == qp_set[0])[0]
-    idxs1 = np.where(qp_train == qp_set[1])[0]
-    idxs2 = np.where(qp_train == qp_set[2])[0]
+    def get_data(self):
+        '''
+        Imports training data into a folder named 'Data'.
+        '''
+        if not os.path.isdir('Data'): os.mkdir('Data')
 
-    idxs = np.concatenate((idxs0,idxs1,idxs2))
+        d0_files = ['Features_Depth_0_Training_1.txt','Features_Depth_0_Training_2.txt',
+        'Features_Depth_0_Training_3.txt','Features_Depth_0_Training_4.txt','Features_Depth_0_Training_5.txt']
 
-    X_train_o = X_train.iloc[idxs,:]
-    y_train_o = y_train.iloc[idxs]
-    costs_train_o = costs_train.iloc[idxs,:]
+        d1_files = ['Features_Depth_1_Training_1.txt', 'Features_Depth_1_Training_2.txt',
+        'Features_Depth_1_Training_3.txt','Features_Depth_1_Training_4.txt','Features_Depth_1_Training_5.txt']
 
-    idxs0 = np.where(qp_test == qp_set[0])[0]
-    idxs1 = np.where(qp_test == qp_set[1])[0]
-    idxs2 = np.where(qp_test == qp_set[2])[0]
+        seqs = list(next(os.walk('FeaturesDataset'))[1])
+        for seq in seqs:
+            qps = list(next(os.walk('FeaturesDataset/' + seq))[1])
+            for qp in qps:
+                df0 = pd.DataFrame()
+                df1 = pd.DataFrame()
+                for d0_file in d0_files:
+                    aux = pd.read_csv('FeaturesDataset/' + seq + '/' + qp + '/HEVC/' + d0_file, sep=' ', names=list(range(25)), header=None)
+                    aux = aux.drop(24,axis=1)
+                    df0 = pd.concat([df0,aux], sort=False, )
+                for d1_file in d1_files:
+                    aux = pd.read_csv('FeaturesDataset/' + seq + '/' + qp + '/HEVC/' + d1_file, sep=' ', names=list(range(25)), header=None)
+                    aux = aux.drop(24,axis=1)
+                    df1 = pd.concat([df1,aux],sort=False)
+                df0.to_csv('Data/'+seq+'_'+qp+'_depth0.csv',sep='|',encoding='utf-8',index=False, )
+                df1.to_csv('Data/'+seq+'_'+qp+'_depth1.csv',sep='|',encoding='utf-8',index=False)
 
-    idxs = np.concatenate((idxs0,idxs1,idxs2))
+    def load_data(self, training_seqs, valid_seqs):
+        '''
+        Loads the data in the class
+        '''
 
-    X_test_o = X_test.iloc[idxs,:]
-    y_test_o = y_test.iloc[idxs]
-    costs_test_o = costs_test.iloc[idxs,:]
+        print('Loading the data...')
 
-    return X_train_o, X_test_o, y_train_o, y_test_o, costs_train_o, costs_test_o
+        df_train = pd.DataFrame()
+        for training_seq in training_seqs:
+            aux = pd.read_csv('Data/'+training_seq,sep='|')
+            df_train = pd.concat([df_train,aux], sort=False)
 
-def isLeaf(line):
-    return len(re.findall('class',line)) != 0
+        df_valid = pd.DataFrame()
+        for valid_seq in valid_seqs:
+            aux = pd.read_csv('Data/'+valid_seq,sep='|')
+            df_valid = pd.concat([df_valid,aux], sort=False)
 
-def get_ntabs(line):
-    return len(re.findall(r'\|   ',line))
+        self.features_train = df_train.iloc[:,1:11]
+        self.classes_train = df_train.iloc[:,11]
+        self.costs_train = df_train.iloc[:,12:23]
 
-def isIf(line):
-    return len(re.findall(r'<=',line)) != 0
+        self.features_valid = df_valid.iloc[:,1:11]
+        self.classes_valid = df_valid.iloc[:,11]
+        self.costs_valid = df_valid.iloc[:,12:23]
 
-def getIf(line):
-    n_tabs = get_ntabs(line)
-    feature = re.findall(r'_\d',line)[0][1]
-    val = re.findall(r'\d+.\d+',line)[0]
-    return '    '*n_tabs + 'if(dFeatures[' + feature + '] <= ' + val + ')\n'
+        if self.simplify_classes:
+            self.classes_train[self.classes_train!=0] = 1
+            self.classes_valid[self.classes_valid!=0] = 1
+            self.simplify_costs()
 
-def getClass(line):
-    return re.findall(r'class: (\d).\d',line)[0]
+    def simplify_costs(self):
 
+        costs0_train = self.costs_train.iloc[:,0].to_frame()
+        costs1_train = self.costs_train.iloc[:,1:].copy()
+        costs1_train[costs1_train<0] = np.nan
+        costs1_train_aux = costs1_train.min(axis=1)
 
-def get_cpp_code(filename):
+        costs0_valid = self.costs_valid.iloc[:,0].to_frame()
+        costs1_valid = self.costs_valid.iloc[:,1:].copy()
+        costs1_valid[costs1_valid<0] = np.nan
+        costs1_valid_aux = costs1_valid.min(axis=1)
 
-    f1 = open('tree.txt','r')
-    lines = f1.read()
-    lines = lines.split('\n')
-    lines = lines[:-1]
-    f1.close()
+        self.costs_train = costs0_train.join(costs1_train_aux.to_frame())
+        self.costs_valid = costs0_valid.join(costs1_valid_aux.to_frame())
 
-    fo = open('tree.cpp','w')
+class Classifier(Data):
+    def __init__(self,max_depth:int=5, random_state:int=42,):
+        Data.__init__(self)
+        self.max_depth=max_depth
+        self.random_state=random_state
+        self.total_cost = 0
+        self.acc = 0
 
-    i=0 #iterador do loop
-    n_lines = len(lines) #numero de linhas no arquivo
-    for line in lines:
-        #se a linha for um 'if'
-        if(isIf(line)):
-            #escreve a if sentence no arquivo
-            fo.write(getIf(line))
-            #somente abre chave depois do if se a próxima linha não for uma folha
-            if(i < n_lines and not isLeaf(lines[i+1])):
-                fo.write('    '*get_ntabs(line)+'{\n')
-        #se a linha for uma folha, so escreve o return
-        elif (isLeaf(line)):
-            fo.write('    '*get_ntabs(line) + 'return ' + getClass(line) + ';\n')
-        #se a linha for um else
-        elif (i < n_lines):
-            #se as linhas anteriores e proxima são leafs, não coloco chave alguma
-            if(isLeaf(lines[i-1]) and isLeaf(lines[i+1])):
-                fo.write('    '*get_ntabs(line) + 'else\n')
-            #caso contrário, fecho chaves antes do else e abro depois dele
+    def fit_tree(self):
+        print('Fitting the decision tree')
+        clf = DecisionTreeClassifier(max_depth=self.max_depth,random_state=self.random_state)
+        clf.fit(self.features_train,self.classes_train)
+        y_pred = clf.predict(self.features_valid)
+        self.acc = clf.score(self.features_valid,self.classes_valid)
+        self.total_cost = self.calculate_cost_of_decisions(y_pred)
+
+    def calculate_cost_of_decisions(self, y_pred):
+        cost = 0
+        for i in range(len(y_pred)):
+            if(y_pred[i] == 0):
+                cost = cost + self.costs_valid.iloc[i,0]
             else:
-                fo.write('    '*(get_ntabs(lines[-1])-2) + '}\n')
-                fo.write('    '*get_ntabs(line) + 'else\n')
-                fo.write('    '*get_ntabs(line) + '{\n')
-        i+=1
-    #fecha a última chave que faltou
-    fo.write('    '*(get_ntabs(lines[-1])-2) + '}')
-    fo.close()
+                cost = cost + self.costs_valid.iloc[i,1]
+        return cost
+
+    def calculate_minimal_cost(self):
+        cost = 0
+        for i in range(len(self.classes_valid)):
+            if(self.classes_valid.iloc[i] == 0):
+                cost = cost + self.costs_valid.iloc[i,0]
+            else:
+                cost = cost + self.costs_valid.iloc[i,1]
+        return cost
