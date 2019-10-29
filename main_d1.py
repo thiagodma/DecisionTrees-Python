29cb57adc58bf3ab898e4fc2b3473e949f973b60
Thiago Dantas <thiagodma52@gmail.com>
2019-10-29 08:23:28 -0300

    new parameters for mains

--- main_d1.py
+++ main_d1.py
@@ -17,21 +17,9 @@ train_QP_37 = ['ChristmasTree_QP_37_depth1.csv','CrowdRun_QP_37_depth1.csv','Duc
 'PedestrianArea_QP_37_depth1.csv','RushHour_QP_37_depth1.csv','Sunflower_QP_37_depth1.csv']
 valid_QP_37 = ['Tractor_QP_37_depth1.csv','Wisley_QP_37_depth1.csv']
 
-'''
-#Later I'll do a 10-fold cross validation so I don't need a explicit validation set
-train_QP_22 = ['ChristmasTree_QP_22_depth1.csv','CrowdRun_QP_22_depth1.csv','DucksTakeOff_QP_22_depth1.csv',
-'PedestrianArea_QP_22_depth1.csv','RushHour_QP_22_depth1.csv','Sunflower_QP_22_depth1.csv','Tractor_QP_22_depth1.csv','Wisley_QP_22_depth1.csv']
-train_QP_27 = ['ChristmasTree_QP_27_depth1.csv','CrowdRun_QP_27_depth1.csv','DucksTakeOff_QP_27_depth1.csv',
-'PedestrianArea_QP_27_depth1.csv','RushHour_QP_27_depth1.csv','Sunflower_QP_27_depth1.csv','Tractor_QP_27_depth1.csv','Wisley_QP_27_depth1.csv']
-train_QP_32 = ['ChristmasTree_QP_32_depth1.csv','CrowdRun_QP_32_depth1.csv','DucksTakeOff_QP_32_depth1.csv',
-'PedestrianArea_QP_32_depth1.csv','RushHour_QP_32_depth1.csv','Sunflower_QP_32_depth1.csv','Tractor_QP_32_depth1.csv','Wisley_QP_32_depth1.csv']
-train_QP_37 = ['ChristmasTree_QP_37_depth1.csv','CrowdRun_QP_37_depth1.csv','DucksTakeOff_QP_37_depth1.csv',
-'PedestrianArea_QP_37_depth1.csv','RushHour_QP_37_depth1.csv','Sunflower_QP_37_depth1.csv','Tractor_QP_37_depth1.csv','Wisley_QP_37_depth1.csv']
-'''
-
 data_QP_22 = Data()
-data_QP_22.load_data(train_QP_22,valid_QP_22)
-clf_QP_22 = Classifier(data_QP_22,max_depth=4,splitter='best',min_samples_leaf=6,criterion='gini')
+data_QP_22.load_data(train_QP_22,valid_QP_22,ftk=[5,6,8])
+clf_QP_22 = Classifier(data_QP_22,max_depth=5)
 clf_QP_22.fit_tree()
 clf_QP_22.prune_duplicate_leaves(clf_QP_22.clf)
 clf_QP_22.get_stats()
@@ -44,8 +32,8 @@ et.write_tree_cpp(1,22,3)
 print('\n')
 
 data_QP_27 = Data()
-data_QP_27.load_data(train_QP_27,valid_QP_27)
-clf_QP_27 = Classifier(data_QP_27, max_depth=8,splitter='best',min_samples_leaf=2,criterion='gini')
+data_QP_27.load_data(train_QP_27,valid_QP_27,ftk=[5,6,8])
+clf_QP_27 = Classifier(data_QP_27, max_depth=1)
 clf_QP_27.fit_tree()
 clf_QP_27.prune_duplicate_leaves(clf_QP_27.clf)
 clf_QP_27.get_stats()
@@ -58,8 +46,8 @@ et.write_tree_cpp(1,27,3)
 print('\n')
 
 data_QP_32 = Data()
-data_QP_32.load_data(train_QP_32,valid_QP_32)
-clf_QP_32 = Classifier(data_QP_32, max_depth=6,splitter='best',min_samples_leaf=2,criterion='gini')
+data_QP_32.load_data(train_QP_32,valid_QP_32,ftk=[5,6,8])
+clf_QP_32 = Classifier(data_QP_32, max_depth=8)
 clf_QP_32.fit_tree()
 clf_QP_32.prune_duplicate_leaves(clf_QP_32.clf)
 clf_QP_32.get_stats()
@@ -72,8 +60,8 @@ et.write_tree_cpp(1,32,3)
 print('\n')
 
 data_QP_37 = Data()
-data_QP_37.load_data(train_QP_37,valid_QP_37)
-clf_QP_37 = Classifier(data_QP_37, max_depth=2,splitter='best',min_samples_leaf=2,criterion='gini')
+data_QP_37.load_data(train_QP_37,valid_QP_37,ftk=[5,6,8])
+clf_QP_37 = Classifier(data_QP_37, max_depth=1)
 clf_QP_37.fit_tree()
 clf_QP_37.prune_duplicate_leaves(clf_QP_37.clf)
 clf_QP_37.get_stats()
