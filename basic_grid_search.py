d51bea336ab591f408698c8640900f3ab4f53112
Thiago Dantas <thiagodma52@gmail.com>
2019-11-05 16:52:05 -0300

    nothing important

--- basic_grid_search.py
+++ basic_grid_search.py
@@ -1,21 +1,21 @@
 from bib import *
 import matplotlib.pyplot as plt
 
-train_QP_22 = ['ChristmasTree_QP_22_depth1.csv','CrowdRun_QP_22_depth1.csv','DucksTakeOff_QP_22_depth1.csv',
-'PedestrianArea_QP_22_depth1.csv','RushHour_QP_22_depth1.csv','Sunflower_QP_22_depth1.csv']
-valid_QP_22 = ['Tractor_QP_22_depth1.csv','Wisley_QP_22_depth1.csv']
+train_QP_22 = ['ChristmasTree_QP_22_depth0.csv','CrowdRun_QP_22_depth0.csv','DucksTakeOff_QP_22_depth0.csv',
+'PedestrianArea_QP_22_depth0.csv','RushHour_QP_22_depth0.csv','Sunflower_QP_22_depth0.csv']
+valid_QP_22 = ['Tractor_QP_22_depth0.csv','Wisley_QP_22_depth0.csv']
 
-train_QP_27 = ['ChristmasTree_QP_27_depth1.csv','CrowdRun_QP_27_depth1.csv','DucksTakeOff_QP_27_depth1.csv',
-'PedestrianArea_QP_27_depth1.csv','RushHour_QP_27_depth1.csv','Sunflower_QP_27_depth1.csv']
-valid_QP_27 = ['Tractor_QP_27_depth1.csv','Wisley_QP_27_depth1.csv']
+train_QP_27 = ['ChristmasTree_QP_27_depth0.csv','CrowdRun_QP_27_depth0.csv','DucksTakeOff_QP_27_depth0.csv',
+'PedestrianArea_QP_27_depth0.csv','RushHour_QP_27_depth0.csv','Sunflower_QP_27_depth0.csv']
+valid_QP_27 = ['Tractor_QP_27_depth0.csv','Wisley_QP_27_depth0.csv']
 
-train_QP_32 = ['ChristmasTree_QP_32_depth1.csv','CrowdRun_QP_32_depth1.csv','DucksTakeOff_QP_32_depth1.csv',
-'PedestrianArea_QP_32_depth1.csv','RushHour_QP_32_depth1.csv','Sunflower_QP_32_depth1.csv']
-valid_QP_32 = ['Tractor_QP_32_depth1.csv','Wisley_QP_32_depth1.csv']
+train_QP_32 = ['ChristmasTree_QP_32_depth0.csv','CrowdRun_QP_32_depth0.csv','DucksTakeOff_QP_32_depth0.csv',
+'PedestrianArea_QP_32_depth0.csv','RushHour_QP_32_depth0.csv','Sunflower_QP_32_depth0.csv']
+valid_QP_32 = ['Tractor_QP_32_depth0.csv','Wisley_QP_32_depth0.csv']
 
-train_QP_37 = ['ChristmasTree_QP_37_depth1.csv','CrowdRun_QP_37_depth1.csv','DucksTakeOff_QP_37_depth1.csv',
-'PedestrianArea_QP_37_depth1.csv','RushHour_QP_37_depth1.csv','Sunflower_QP_37_depth1.csv']
-valid_QP_37 = ['Tractor_QP_37_depth1.csv','Wisley_QP_37_depth1.csv']
+train_QP_37 = ['ChristmasTree_QP_37_depth0.csv','CrowdRun_QP_37_depth0.csv','DucksTakeOff_QP_37_depth0.csv',
+'PedestrianArea_QP_37_depth0.csv','RushHour_QP_37_depth0.csv','Sunflower_QP_37_depth0.csv']
+valid_QP_37 = ['Tractor_QP_37_depth0.csv','Wisley_QP_37_depth0.csv']
 
 trains = [train_QP_22, train_QP_27, train_QP_32, train_QP_37]
 valids = [valid_QP_22, valid_QP_27, valid_QP_32, valid_QP_37]
@@ -28,19 +28,19 @@ best_parameters = []
 qps = [22,27,32,37]
 for train,valid,qp in zip(trains,valids,qps):
     data = Data()
-    if qp == 27 or qp==37:
-        data.load_data(train,valid,ftk=[5,6,8])
-    else:
-        data.load_data(train,valid)
+    #if qp == 27 or qp==37:
+    #    data.load_data(train,valid,ftk=[5,6,8])
+    #else:
+    data.load_data(train,valid)
     accs = []
     costs = []
     for depth in depths:
 
-        if qp == 22:
-            clf = Classifier(data,max_depth=depth)
-        else:
-            clf = Classifier(data,max_depth=depth,hack=True)
-        clf.fit_tree()
+        #if qp == 22:
+        clf = Classifier(data,max_depth=depth)
+        #else:
+        #    clf = Classifier(data,max_depth=depth,hack=True)
+        #clf.fit_tree()
         clf.fit_tree()
         clf.prune_duplicate_leaves(clf.clf)
         clf.get_stats()
--- main_d0.py
+++ main_d0.py
@@ -19,7 +19,7 @@ valid_QP_37 = ['Tractor_QP_37_depth0.csv','Wisley_QP_37_depth0.csv']
 #checks if there is a 'tree.cpp' file. If so, deletes it
 if os.path.isfile('tree.cpp'): os.remove('tree.cpp')
 
-max_depths = [7,3,5,7]
+max_depths = [7,3,3,7]
 trains = [train_QP_22, train_QP_27, train_QP_32, train_QP_37]
 valids = [valid_QP_22, valid_QP_27, valid_QP_32, valid_QP_37]
 qps = [22,27,32,37]
@@ -27,14 +27,14 @@ qps = [22,27,32,37]
 for train,valid,max_depth,qp in zip(trains,valids,max_depths,qps):
 
     data = Data()
-    if qp == 27 or qp==37:
-        data.load_data(train,valid,ftk=[1,3,5,6,7,8])
-    else:
-        data.load_data(train,valid)
-    if qp == 22:
-        clf = Classifier(data,max_depth=max_depth)
-    else:
-        clf = Classifier(data,max_depth=max_depth,hack=True)
+    #if qp == 27 or qp==37:
+    #    data.load_data(train,valid,ftk=[1,3,5,6,7,8])
+    #else:
+    data.load_data(train,valid)
+    #if qp == 22:
+    clf = Classifier(data,max_depth=max_depth)
+    #else:
+    #    clf = Classifier(data,max_depth=max_depth,hack=True)
     clf.fit_tree()
     clf.prune_duplicate_leaves(clf.clf)
     clf.get_stats()
